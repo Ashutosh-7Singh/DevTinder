0fa05e13176955973ff0f29e90133549bd5579d9
Ashutosh-7Singh <ashutosh.singh@thegoacademy.com>
2024-10-02 00:35:13 +0530

    MultiRoute

--- devTinder_Backend/src/app.js
+++ devTinder_Backend/src/app.js
@@ -1,10 +1,45 @@
 const express = require("express");
-const app=express();
+const app = express();
+//  we can do like this 
+app.use("/user", (req, res, next) => {
+  console.log("Handling the route user1!!");
+  //   res.send("Route Handler 1!");
+  next()
+  },(req, res, next) => {
+    console.log("Handling the route user 2 !!");
+    // res.send("Router Hander 2 !!")
+    next();
+},
+(req,res,next)=>{
+    console.log("Handling the route user 3 !!");
+    // res.send("Router Handler 3 !!)
+    next();
+}
 
-app.post("/user",(req,res)=>{
-//Route Handler 
-res.send("Route Handler 1!");
-})
+);
 
-app.listen(7777,()=>{    console.log('Server is listning on port 777..... ')
-})
+
+// or we can all in one array also we can put any two in one array 
+
+app.use("/user",
+    [ (req, res, next) => {
+    console.log("Handling the route user1!!");
+    //   res.send("Route Handler 1!");
+    next()
+    },(req, res, next) => {
+      console.log("Handling the route user 2 !!");
+      // res.send("Router Hander 2 !!")
+      next();
+  },
+  (req,res,next)=>{
+      console.log("Handling the route user 3 !!");
+      // res.send("Router Handler 3 !!)
+      next();
+  }
+]
+  );
+  
+
+app.listen(7777, () => {
+  console.log("Server is listning on port 777..... ");
+});
